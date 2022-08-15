c8d3f29aa4865d2799e4da5bedd82500830efc81
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-15 10:57:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor((Math.random() * 1000) + 1);
+
+let num3 = 13;
+let num4 = 9;
+let mod = num3 % num4;
+
+let max = Math.max(1,5,10,20);
